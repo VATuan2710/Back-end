491b66e1d916384cd7758e3441358183df88a2e7
VATuan2710 <vatbgct@gmail.com>
2025-01-11 17:07:33 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import mongoose from "mongoose";
+import routes from "./src/routes/index.js";
+import connectDB from "./src/config/db.js";
+import dotenv from "dotenv";
+const app = express();
+
+dotenv.config();
+const { PORT } = process.env;
+app.use(express.json());
+
+connectDB();
+
+// app.get("/products", async (req, res) => {});
+
+// app.get("/products/:id", async (req, res) => {});
+
+// app.delete("/products/:id", async (req, res) => {});
+
+// app.post("/products", async (req, res) => {});
+
+// app.patch("/products/:id", async (req, res) => {});
+
+app.use("/", routes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
---
+++ src/routes/productRoutes.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import {
+  create,
+  getAll,
+  getById,
+  removeById,
+  updateById,
+} from "../controllers/productController.js";
+
+const productRoutes = Router();
+
+productRoutes.get("/", getAll);
+productRoutes.get("/:id", getById);
+productRoutes.post("/", create);
+productRoutes.patch("/:id", updateById);
+productRoutes.delete("/:id", removeById);
+
+export default productRoutes;
